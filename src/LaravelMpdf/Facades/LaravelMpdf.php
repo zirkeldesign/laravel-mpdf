138185e59736a9d119c6c7524b8561ce65c6aa10
Daniel Sturm <d.sturm@zirkeldesign.de>
2020-09-02 09:46:23 +0200

    style: :art: Clean up
    Add .editorconfig, phpcs.xml and fix files.

--- src/LaravelMpdf/Facades/LaravelMpdf.php
+++ src/LaravelMpdf/Facades/LaravelMpdf.php
@@ -4,14 +4,16 @@ namespace Meneses\LaravelMpdf\Facades;
 
 use Illuminate\Support\Facades\Facade as BaseFacade;
 
-class LaravelMpdf extends BaseFacade {
+class LaravelMpdf extends BaseFacade
+{
 
-	/**
-	 * Get the registered name of the component.
-	 *
-	 * @return string
-	 */
-	protected static function getFacadeAccessor() { 
-		return 'mpdf.wrapper'; 
-	}
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'mpdf.wrapper';
+    }
 }
